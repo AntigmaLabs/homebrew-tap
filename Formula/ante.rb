c9e12b9d73b2a4aa6319c95dc3f9b34b79702851
mohanz <m68zhang@gmail.com>
2025-02-23 11:26:42 -0800

    new version

--- Formula/ante.rb
+++ Formula/ante.rb
@@ -9,7 +9,7 @@ class Ante < Formula
     if OS.mac?
       if Hardware::CPU.arm?
         url "https://github.com/#{repo}/releases/download/v#{version}/ante-v#{version}-Darwin-arm64.tar.gz"
-        sha256 "c178ba9f2de2552831d5a50283614b1dbe093babca24684313c86f527225a568"
+        sha256 "a3fa928056fc024d7c4c2a3767d635c3c8684f6140d40060fc3d8c4ce96601ac"
       else
         url "https://github.com/#{repo}/releases/download/v#{version}/ante-v#{version}-Darwin-x86_64.tar.gz"
         # sha256 "THE_HASH_FROM_YOUR_X86_64_RELEASE"
