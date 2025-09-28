d0f9a46de967bcace2a13cc0cbfb03ec0d1acb7f
xiedaxia1hao <hangxiee@gmail.com>
2025-09-28 16:34:36 -0700

    ante 0.1.5 (binary) Auto-bumped to https://github.com/AntigmaLabs/test_release/releases/download/v0.1.5/ante

--- Formula/ante.rb
+++ Formula/ante.rb
@@ -1,15 +1,15 @@
 class Ante < Formula
     desc "Building substrate for self-organizing intelligence"
     homepage "https://github.com/AntigmaLabs"
-    version "0.1.3" # Match your current version
+    version "0.1.5" # Match your current version
   
     # Add repo name variable
     repo = "AntigmaLabs/homebrew-tap"
 
     if OS.mac?
       if Hardware::CPU.arm?
-        url "https://github.com/#{repo}/releases/download/v#{version}/ante"
-        sha256 "sha256:3a9d16520981480df0217bcce067a360383cf68ad397cd4d8b6e294fb1d4b99b"
+        url "https://github.com/AntigmaLabs/test_release/releases/download/v0.1.5/ante"
+        sha256 "3c56513d7fdf34285963ca0aa95624f5f00cc78938ea4dbb27217918bba49698"
       else
         url "https://github.com/#{repo}/releases/download/v#{version}/ante-v#{version}-Darwin-x86_64.tar.gz"
         # sha256 "THE_HASH_FROM_YOUR_X86_64_RELEASE"
