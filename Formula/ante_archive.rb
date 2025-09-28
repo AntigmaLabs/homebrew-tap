1d3429f39b9c0ad2340f885939abecdd6399f657
Hang <hangxiee@gmail.com>
2025-09-28 12:05:26 -0700

    update rb file

--- Formula/ante_archive.rb
+++ Formula/ante_archive.rb
@@ -1,15 +1,15 @@
 class Ante < Formula
     desc "Building substrate for self-organizing intelligence"
     homepage "https://github.com/AntigmaLabs"
-    version "0.1.3" # Match your current version
+    version "0.0.0" # Match your current version
   
     # Add repo name variable
     repo = "AntigmaLabs/homebrew-tap"
 
     if OS.mac?
       if Hardware::CPU.arm?
-        url "https://github.com/#{repo}/releases/download/v#{version}/ante"
-        sha256 "sha256:3a9d16520981480df0217bcce067a360383cf68ad397cd4d8b6e294fb1d4b99b"
+        url "https://github.com/#{repo}/releases/download/v#{version}/ante-v#{version}-Darwin-arm64.tar.gz"
+        sha256 "d03a7bff5ef722468a8ce5f1382e835a2f955c5de14e4d3f8b684c1e46124678"
       else
         url "https://github.com/#{repo}/releases/download/v#{version}/ante-v#{version}-Darwin-x86_64.tar.gz"
         # sha256 "THE_HASH_FROM_YOUR_X86_64_RELEASE"
